376d388cb03e6b1a38bd92dd6893a804b4cc6efe
Ronald Luitwieler (MacBookie Pro) <ronald.luitwieler@gmail.com>
2013-12-02 10:34:13 +0100

    renaming file

---
+++ Classes/ios/OWLog.h
@@ -0,0 +1,11 @@
+
+// The DLog macro is used to only output when the DEBUG variable is set (-DDEBUG in the projects's C flags for the debug confirguration: edit scheme).
+#ifdef DEBUG
+#   define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#   define DLog(...)
+#endif
+
+
+// ALog always displays output regardless of the DEBUG setting
+#define ALog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
